ec480d1e5cba6515a47dba4c52d5389b838d0688
Vladimirova Tetiana <vladimirovatanya1107@gmail.com>
2025-01-24 01:18:57 +0200

    add footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import RouterComp from './components/Router/RouterComp';
 import Header from './components/Layout/Header/Header';
 import Sidebar from './components/Layout/Sidebar/Sidebar';
+import Footer from './components/Layout/Footer/Footer'
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
             <RouterComp />
           </div>
         </Router>
+        <Footer />
       </div>
     </div>
   );
---
+++ frontend/src/components/Layout/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+function Footer() {
+  return (
+    <div>
+      <footer className="fixed bottom-[40px] left-[40px] w-auto  bg-white">
+        <p className="text-left text-[16px] font-aeroport font-400 text-[#AAB4B6]">Log out</p>
+      </footer>
+    </div>
+  );
+}
+
+export default Footer;
