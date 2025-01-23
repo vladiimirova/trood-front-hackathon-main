ec4353b0b22c88a7285488e041325c64aef6ce04
Vladimirova Tetiana <vladimirovatanya1107@gmail.com>
2025-01-24 00:41:04 +0200

    fixed icons

--- frontend/src/components/Layout/Header/HeaderComp/Menu.jsx
+++ frontend/src/components/Layout/Header/HeaderComp/Menu.jsx
@@ -1,9 +1,9 @@
 function Menu() {
     return (
       <div className="flex justify-between items-center gap-[16px]">
-       <img src="./icons/iconoir_message-solid.svg" alt="iconoir_message-solid" className="mr-[8px]"/>
-       <img src="./icons/mingcute_notification-fill.svg" alt="mingcute_notification-fill" className="mr-[10px]"/>
-       <img src="./icons/Ellipse.svg" alt="Ellipse" />
+       <img src="/icons/iconoir_message-solid.svg" alt="iconoir_message-solid" className="mr-[8px]"/>
+       <img src="/icons/mingcute_notification-fill.svg" alt="mingcute_notification-fill" className="mr-[10px]"/>
+       <img src="/icons/Ellipse.svg" alt="Ellipse" />
        <p className="text-[16px]font-aeroport font-400 whitespace-nowrap">Alex Smith</p>
       </div>
     );
